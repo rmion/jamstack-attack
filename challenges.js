b78f2603505338ae507ccaf72c379fabcdf5f479
Robert Mion <mr.robert.mion@gmail.com>
2021-01-06 19:17:45 -0500

    Doubled number of challenges

--- challenges.js
+++ challenges.js
@@ -59,4 +59,64 @@ module.exports = [
         "topic": "html",
         "code": "<dl></dl>",
     },
+    {
+        "topic": "js",
+        "code": "$('#twitter').hide()"
+    },
+    {
+        "topic": "html",
+        "code": "<time datetime='05/21/2021'></time>"
+    },
+    {
+        "topic": "css",
+        "code": "p { background-color: red; }"
+    },
+    {
+        "topic": "js",
+        "code": "window.location.protocol === 'https:'"
+    },
+    {
+        "topic": "html",
+        "code": "<img src='sample.jpg' alt='' />"
+    },
+    {
+        "topic": "css",
+        "code": "a:active, a:focus, a:hover"
+    },
+    {
+        "topic": "js",
+        "code": "[1,2,3].length"
+    },
+    {
+        "topic": "html",
+        "code": "<p>I am <strong>here</strong></p>"
+    },
+    {
+        "topic": "css",
+        "code": "input:required"
+    },
+    {
+        "topic": "js",
+        "code": "document.form[0].submit()"
+    },
+    {
+        "topic": "html",
+        "code": "<link rel='stylesheet' href='styles.css' />"
+    },
+    {
+        "topic": "css",
+        "code": "li > a"
+    },
+    {
+        "topic": "js",
+        "code": "document.getElementById('myText').style.fontSize = '20'"
+    },
+    {
+        "topic": "html",
+        "code": "<th scope='col'>Player</th>"
+    },
+    {
+        "topic": "css",
+        "code": "h2 ~ p"
+    },
 ]
