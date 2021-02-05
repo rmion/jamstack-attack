bad9ec127894ed643ca295ce63a421cf9cbac536
Robert Mion <mr.robert.mion@gmail.com>
2021-02-05 10:30:00 -0500

    A few more grab n go exercises

--- querying.js
+++ querying.js
@@ -1,25 +1,64 @@
 module.exports = [
     {
-        "blob": { 
-            "name": "Robert",
-            "age": 33,
-            "list": [
-              1,
-              2,
-              3
-            ]
-          },
-        "item": 33
+      "blob": { 
+          "name": "Robert",
+          "age": 33,
+          "list": [
+            1,
+            2,
+            3
+          ]
+        },
+      "item": 33
     },
     {
-        "blob": { 
-            "name": "To-do",
-            "list": [
-              1,
-              2,
-              3
-            ]
-          },
-        "item": 3
+      "blob": {
+        "word": "example",
+        "synonyms": [
+          "model",
+          "exemplar",
+          "good example",
+          "deterrent example",
+          "lesson",
+          "object lesson",
+          "exercise"
+        ]
+      },
+      "item": "good"
     },
+    {
+      "blob": {
+        "languages": {
+            "iso639_1": "es",
+            "iso639_2": "spa",
+            "name": "Spanish",
+            "nativeName": "Español"
+        },
+        "translations": {
+            "de": "Kolumbien",
+            "es": "Colombia",
+            "fr": "Colombie",
+            "ja": "コロンビア",
+            "it": "Colombia",
+            "br": "Colômbia",
+            "pt": "Colômbia"
+        }
+      },
+      "item": "Colombie"
+    },
+    {
+      "blob": {
+        "capital": "Bogotá",
+        "altSpellings": [
+          "CO", 
+          "Republic of Colombia", 
+          "República de Colombia"
+        ],
+        "region": "Americas",
+        "subregion": "South America",
+        "population": 48759958,
+        "latlng": [4.0, -72.0],
+      },
+      "item": -72.0
+    }
 ]
