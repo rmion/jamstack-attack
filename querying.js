cb4f7b063410bcc43f2b780b543292daab16daad
Robert Mion <mr.robert.mion@gmail.com>
2021-02-08 13:20:28 -0500

    Renamed object game, added exercises

--- querying.js
+++ querying.js
@@ -1,15 +1,14 @@
 module.exports = [
     {
       "blob": { 
-          "name": "Robert",
-          "age": 33,
           "list": [
             1,
             2,
             3
           ]
         },
-      "item": 33
+      "item": 33,
+      "code": ".list[2] + String(obj.list[2])"
     },
     {
       "blob": {
@@ -18,13 +17,11 @@ module.exports = [
           "model",
           "exemplar",
           "good example",
-          "deterrent example",
-          "lesson",
-          "object lesson",
-          "exercise"
+          "deterrent example"
         ]
       },
-      "item": "good"
+      "item": "good",
+      "code": ".synonyms[2].slice(0,4)"
     },
     {
       "blob": {
@@ -37,14 +34,11 @@ module.exports = [
         "translations": {
             "de": "Kolumbien",
             "es": "Colombia",
-            "fr": "Colombie",
-            "ja": "コロンビア",
-            "it": "Colombia",
-            "br": "Colômbia",
-            "pt": "Colômbia"
+            "fr": "Colombie"
         }
       },
-      "item": "Colombie"
+      "item": "Colombie",
+      "code": ".translations.fr"
     },
     {
       "blob": {
@@ -59,13 +53,67 @@ module.exports = [
         "population": 48759958,
         "latlng": [4.0, -72.0],
       },
-      "item": -72.0
+      "item": -72.0,
+      "code": ".latlng[1]"
     },
     {
       "blob": {
         "Two-face": "Joker",
         "Bat man": "Bruce Wayne"
       },
-      "item": "Joker"
-    }
+      "item": "Joker",
+      "code": "['Two-face']"
+    },
+    {
+      "blob": {
+        "1": "abc"
+      },
+      "item": "B",
+      "code": "[1][1].toUpperCase()"
+    },
+    {
+      "blob": {
+        "0": "one"
+      },
+      "item": "1",
+      "code": "[0].indexOf('n')"
+    },
+    {
+      "blob": {
+        "foo": 21,
+        "bar": 13
+      },
+      "item": 8,
+      "code": ".foo - obj.bar"
+    },
+    {
+      "blob": {
+        "fun": 4,
+        "day": 4
+      },
+      "item": true,
+      "code": ".fun == obj.day"
+    },
+    {
+      "blob": {
+        "list": [1,2,4]
+      },
+      "item": 3,
+      "code": ".list.length"
+    },
+    {
+      "blob": {
+        "left": [3,2,0],
+        "right": [1,2,3]
+      },
+      "item": "3,2,1,2,3",
+      "code": ".left.slice(0,2).concat(obj.right).toString()"
+    },
+    {
+      "blob": {
+        "bond": [0,0,14]
+      },
+      "item": "[0,0,7]",
+      "code": ".bond.map(i => i / 2)"
+    },
 ]
