b7d1be2ba3a11a11da47605e7be52fa2548b3176
Robert Mion <mr.robert.mion@gmail.com>
2021-02-08 20:26:44 -0500

    Added clues to Object Puzzles

--- public/query.js
+++ public/query.js
@@ -1,14 +1,28 @@
-let obj, item, itemAsString;
+let obj, item, itemAsString, clues, solution;
 
 function fetchGame(e) {
     fetch('/object')
         .then(response => response.json())
         .then(data => {
+          document.getElementById('requirements').innerHTML = ""
             e.target.setAttribute('disabled', true)
             document.getElementById('player-1').classList.remove('is-hidden')
             obj = data.blob
             itemAsString = JSON.stringify(data.blob, null, 4)
             item = data.item
+            clues = data.methods
+            answer = data.code
+            if (clues.length) {
+              document.getElementById('requirements').textContent = "Your solution should include: "
+              clues.forEach(clue => {
+                let span = document.createElement('span')
+                span.innerHTML = `
+                  <input type="checkbox" id="${clue}" />
+                  <a href='https://developer.mozilla.org/en-US/search?q=${clue}'>${clue}</a>
+                `
+                document.getElementById('requirements').appendChild(span)
+              })  
+            }
             document.getElementById('challenge').textContent = "const obj = " + itemAsString
             document.getElementById('result').textContent = ""
             document.getElementById('expectation').textContent = item
@@ -23,6 +37,15 @@ document.getElementById('start').addEventListener('click', fetchGame)
   
   document.getElementById('query-answer').addEventListener('input', function(e)  {
     document.getElementById('notification').textContent = ""
+    if (clues.length) {
+      clues.forEach(clue => {
+        if (e.target.value.indexOf(clue) > 0) {
+          document.getElementById(clue).setAttribute('checked', true)
+        } else {
+          document.getElementById(clue).removeAttribute('checked')
+        }
+      })  
+    }
       try {
         if (eval('obj' + e.target.value) == item) {
               document.getElementById('result').textContent = JSON.stringify(Function('"use strict";return (obj' + e.target.value + ')')(), null, 4)
--- querying.js
+++ querying.js
@@ -8,7 +8,10 @@ module.exports = [
           ]
         },
       "item": 33,
-      "code": ".list[2] + String(obj.list[2])"
+      "code": ".list[2].toString() + obj.list[2]",
+      "methods": [
+        "toString"
+      ]
     },
     {
       "blob": {
@@ -21,40 +24,40 @@ module.exports = [
         ]
       },
       "item": "good",
-      "code": ".synonyms[2].slice(0,4)"
+      "code": ".synonyms[2].slice(0,4)",
+      "methods": [
+        "slice"
+      ]
     },
     {
       "blob": {
-        "languages": {
-            "iso639_1": "es",
-            "iso639_2": "spa",
-            "name": "Spanish",
-            "nativeName": "Español"
-        },
         "translations": {
             "de": "Kolumbien",
             "es": "Colombia",
             "fr": "Colombie"
         }
       },
-      "item": "Colombie",
-      "code": ".translations.fr"
+      "item": 9,
+      "code": ".translations.de.length",
+      "methods": [
+        "length"
+      ]
     },
     {
       "blob": {
         "capital": "Bogotá",
-        "altSpellings": [
-          "CO", 
-          "Republic of Colombia", 
-          "República de Colombia"
-        ],
         "region": "Americas",
         "subregion": "South America",
-        "population": 48759958,
+        "popul": 48759958,
         "latlng": [4.0, -72.0],
       },
-      "item": -72.0,
-      "code": ".latlng[1]"
+      "item": 55,
+      "code": ".popul.toString().split('').reduce((a,i) => { return a + +i }, 0)",
+      "methods": [
+        "toString",
+        "split",
+        "reduce"
+      ]
     },
     {
       "blob": {
@@ -62,21 +65,30 @@ module.exports = [
         "Bat man": "Bruce Wayne"
       },
       "item": "Joker",
-      "code": "['Two-face']"
+      "code": "['Bat man'].split(' ')[0]",
+      "methods": [
+        'split'
+      ]
     },
     {
       "blob": {
         "1": "abc"
       },
       "item": "B",
-      "code": "[1][1].toUpperCase()"
+      "code": "[1][1].toUpperCase()",
+      "methods": [
+        "toUpperCase"
+      ]
     },
     {
       "blob": {
         "0": "one"
       },
       "item": "1",
-      "code": "[0].indexOf('n')"
+      "code": "[0].indexOf('n')",
+      "methods": [
+        "indexOf"
+      ]
     },
     {
       "blob": {
@@ -84,7 +96,8 @@ module.exports = [
         "bar": 13
       },
       "item": 8,
-      "code": ".foo - obj.bar"
+      "code": ".foo - obj.bar",
+      "methods": []
     },
     {
       "blob": {
@@ -92,14 +105,18 @@ module.exports = [
         "day": 4
       },
       "item": true,
-      "code": ".fun == obj.day"
+      "code": ".fun == obj.day",
+      "methods": []
     },
     {
       "blob": {
         "list": [1,2,4]
       },
-      "item": 3,
-      "code": ".list.length"
+      "item": 12,
+      "code": ".list.length * list[2]",
+      "methods": [
+        "length"
+      ]
     },
     {
       "blob": {
@@ -107,13 +124,21 @@ module.exports = [
         "right": [1,2,3]
       },
       "item": "3,2,1,2,3",
-      "code": ".left.slice(0,2).concat(obj.right).toString()"
+      "code": ".left.slice(0,2).concat(obj.right)",
+      "methods": [
+        "slice",
+        "concat",
+        "toString"
+      ]
     },
     {
       "blob": {
         "bond": [0,0,14]
       },
-      "item": "[0,0,7]",
-      "code": ".bond.map(i => i / 2)"
+      "item": "0,0,7",
+      "code": ".bond.map(i => i / 2)",
+      "methods": [
+        "map"
+      ]
     },
 ]
